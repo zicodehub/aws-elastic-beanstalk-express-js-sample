818458240075c0a3539b6251d583a8d65935a9fc
zicode <marcleord@gmail.com>
2021-09-02 18:15:55 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const port = 8080;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+console.log(`App -marche- running on http://localhost:${port}`);
