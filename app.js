18715dbf47aec81b66f9685a1d39ac519ad7f4ee
zicode <marcleord@gmail.com>
2021-09-02 18:37:09 +0000

    Second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! Bonjoiur les gars'));
 
 app.listen(port);
 console.log(`App -marche- running on http://localhost:${port}`);
