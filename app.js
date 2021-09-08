250da78d98ab01505d6c35008ac3a201415f8526
zicode <marcleord@gmail.com>
2021-09-08 19:25:56 +0000

    Second change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Bonjoiur les gars'));
+app.get('/', (req, res) => res.send('Je suis un devOps !!! Terminé, chui un goat!!!!!!'));
 
 app.listen(port);
 console.log(`App -marche- running on http://localhost:${port}`);
